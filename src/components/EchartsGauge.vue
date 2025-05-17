01b3116173a93ceaab4d9ad94810096e11585fbc
usuario <170405410+mgrl39@users.noreply.github.com>
2025-05-17 03:43:52 +0200

    3. Importa y registra vue-echarts en el componente.

--- src/components/EchartsGauge.vue
+++ src/components/EchartsGauge.vue
@@ -1,5 +1,15 @@
 <template></template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// Echarts
+import { use } from "echarts/core";
+import VEChart from "vue-echarts";
+import { GaugeChart } from "echarts/charts";
+import { CanvasRenderer } from "echarts/renderers";
+import { TooltipComponent } from "echarts/components";
+
+// Registrar los módulos necesarios de ECharts
+use([GaugeChart, CanvasRenderer, TooltipComponent]);
+</script>
 
 <style scoped></style>
