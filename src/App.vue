1fb1bd18702cfd552930f1cb46f29f2c393a06b1
usuario <170405410+mgrl39@users.noreply.github.com>
2025-05-27 09:02:21 +0200

    prettier some files

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,26 @@
             <ion-list-header>Dashboard</ion-list-header>
             <ion-note>Data Visualization</ion-note>
 
-
-            <ion-menu-toggle :auto-hide="false" v-for="(p, i) in appPages" :key="i">
-              <ion-item @click="selectedIndex = i" router-direction="root" :router-link="p.url" lines="none" :detail="false" class="hydrated" :class="{ selected: selectedIndex === i }">
-                <ion-icon aria-hidden="true" slot="start" :ios="p.iosIcon" :md="p.mdIcon"></ion-icon>
+            <ion-menu-toggle
+              :auto-hide="false"
+              v-for="(p, i) in appPages"
+              :key="i"
+            >
+              <ion-item
+                @click="selectedIndex = i"
+                router-direction="root"
+                :router-link="p.url"
+                lines="none"
+                :detail="false"
+                class="hydrated"
+                :class="{ selected: selectedIndex === i }"
+              >
+                <ion-icon
+                  aria-hidden="true"
+                  slot="start"
+                  :ios="p.iosIcon"
+                  :md="p.mdIcon"
+                ></ion-icon>
                 <ion-label>{{ p.title }}</ion-label>
               </ion-item>
             </ion-menu-toggle>
@@ -22,80 +38,84 @@
   </ion-app>
 </template>
 
-
 <script setup lang="ts">
-import { IonApp, IonContent, IonIcon, IonItem, IonLabel, IonList, IonListHeader, IonMenu, IonMenuToggle, IonNote, IonRouterOutlet, IonSplitPane,} from '@ionic/vue';
-import { rocketOutline, rocketSharp, pulseOutline, pulseSharp, speedometerOutline, speedometerSharp } from 'ionicons/icons';
-import { ref, onMounted, watch } from 'vue';
-import { useRoute } from 'vue-router';
-
+import {
+  IonApp,
+  IonContent,
+  IonIcon,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonListHeader,
+  IonMenu,
+  IonMenuToggle,
+  IonNote,
+  IonRouterOutlet,
+  IonSplitPane,
+} from "@ionic/vue";
+import {
+  rocketOutline,
+  rocketSharp,
+  pulseOutline,
+  pulseSharp,
+  speedometerOutline,
+  speedometerSharp,
+} from "ionicons/icons";
+import { ref, onMounted, watch } from "vue";
+import { useRoute } from "vue-router";
 
 const selectedIndex = ref(0);
 const appPages = [
   {
-    title: 'Negocio',
-    url: '/negocio',
+    title: "Negocio",
+    url: "/negocio",
     iosIcon: rocketOutline,
     mdIcon: rocketSharp,
   },
   {
-    title: 'Técnico',
-    url: '/tecnico',
+    title: "Técnico",
+    url: "/tecnico",
     iosIcon: pulseOutline,
     mdIcon: pulseSharp,
   },
   {
-    title: 'KPIs',
-    url: '/kpis',
+    title: "KPIs",
+    url: "/kpis",
     iosIcon: speedometerOutline,
     mdIcon: speedometerSharp,
   },
 ];
 
-
-
-
 const route = useRoute();
-
 
 /********************************************************************** */
 // 🔄 Función para actualizar el `selectedIndex` según la URL actual
 
-
 const updateSelectedIndex = () => {
   const currentPath = route.path;
-  const index = appPages.findIndex(page => page.url === currentPath);
+  const index = appPages.findIndex((page) => page.url === currentPath);
   if (index !== -1) {
     selectedIndex.value = index;
   }
 };
 
-
 // Ejecutar cuando la app carga
 onMounted(updateSelectedIndex);
-
 
 // Ejecutar cada vez que cambia la ruta
 watch(route, updateSelectedIndex);
 /********************************************************************** */
-
-
 </script>
 
-
 <style scoped>
-
-
 ion-split-pane {
-    /*--side-width: 350px;*/
-    --side-max-width: 280px;
+  /*--side-width: 350px;*/
+  --side-max-width: 280px;
 }
-
 
 ion-menu ion-content {
   --background: var(--ion-item-background, var(--ion-background-color, #fff));
 }
-
 
 ion-menu.md ion-content {
   --padding-start: 8px;
@@ -104,50 +124,39 @@ ion-menu.md ion-content {
   --padding-bottom: 20px;
 }
 
-
 ion-menu.md ion-list {
   padding: 20px 0;
 }
 
-
 ion-menu.md ion-note {
   margin-bottom: 30px;
 }
-
 
 ion-menu.md ion-list-header,
 ion-menu.md ion-note {
   padding-left: 10px;
 }
 
-
 ion-menu.md ion-list#inbox-list {
   border-bottom: 1px solid var(--ion-background-color-step-150, #d7d8da);
 }
-
 
 ion-menu.md ion-list#inbox-list ion-list-header {
   font-size: 22px;
   font-weight: 600;
 
-
   min-height: 20px;
 }
-
 
 ion-menu.md ion-list#labels-list ion-list-header {
   font-size: 16px;
 
-
   margin-bottom: 18px;
-
 
   color: #757575;
 
-
   min-height: 26px;
 }
-
 
 ion-menu.md ion-item {
   --padding-start: 10px;
@@ -155,42 +164,34 @@ ion-menu.md ion-item {
   border-radius: 4px;
 }
 
-
 ion-menu.md ion-item.selected {
   --background: rgba(var(--ion-color-primary-rgb), 0.14);
 }
-
 
 ion-menu.md ion-item.selected ion-icon {
   color: var(--ion-color-primary);
 }
 
-
 ion-menu.md ion-item ion-icon {
   color: #616e7e;
 }
-
 
 ion-menu.md ion-item ion-label {
   font-weight: 500;
 }
 
-
 ion-menu.ios ion-content {
   --padding-bottom: 20px;
 }
-
 
 ion-menu.ios ion-list {
   padding: 20px 0 0 0;
 }
 
-
 ion-menu.ios ion-note {
   line-height: 24px;
   margin-bottom: 20px;
 }
-
 
 ion-menu.ios ion-item {
   --padding-start: 16px;
@@ -198,22 +199,18 @@ ion-menu.ios ion-item {
   --min-height: 50px;
 }
 
-
 ion-menu.ios ion-item.selected ion-icon {
   color: var(--ion-color-primary);
 }
-
 
 ion-menu.ios ion-item ion-icon {
   font-size: 24px;
   color: #73849a;
 }
 
-
 ion-menu.ios ion-list#labels-list ion-list-header {
   margin-bottom: 8px;
 }
-
 
 ion-menu.ios ion-list-header,
 ion-menu.ios ion-note {
@@ -221,20 +218,16 @@ ion-menu.ios ion-note {
   padding-right: 16px;
 }
 
-
 ion-menu.ios ion-note {
   margin-bottom: 8px;
 }
-
 
 ion-note {
   display: inline-block;
   font-size: 16px;
 
-
   color: var(--ion-color-medium-shade);
 }
-
 
 ion-item.selected {
   --color: var(--ion-color-primary);
